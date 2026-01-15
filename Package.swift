ed1e2caceb8a1fd4bedd9da50ebb898eeb0b1f2c
jinguihua <156711203@qq.com>
2026-01-15 16:06:55 +0800

    chagned

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "myMMWormhol",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,7 +20,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "myMMWormhol"
+            name: "myMMWormhol",
+            path: "Sources/myMMWormhol",
+            publicHeadersPath: "."   // ⭐ 非常关键 告诉 SwiftPM：这些 .h 都是对外公开的
         ),
         .testTarget(
             name: "myMMWormholTests",
